e88e808039b6153821601111565630755a10c56f
Théo Winterhalter <isheeft@gmail.com>
2018-06-11 11:14:46 +0200

    nice predicate is now true for every derivation (not intended, WIP)

--- theories/XTyping.v
+++ theories/XTyping.v
@@ -306,7 +306,98 @@ with nice_eq_term Σ : forall Γ t u A, Σ ;;; Γ |-x t = u : A -> Type :=
     nice_eq_term _ _ _ _ _ h2 ->
     nice_eq_term _ _ _ _ _ (eq_transitivity Σ Γ u v w A h1 h2)
 
-(* Is this really the right way? *)
+| nice_beta Γ s1 s2 n A B t u hA hB ht hu :
+    nice_typing _ _ _ _ hA ->
+    nice_typing _ _ _ _ hB ->
+    nice_typing _ _ _ _ ht ->
+    nice_typing _ _ _ _ hu ->
+    nice_eq_term _ _ _ _ _ (eq_beta Σ Γ s1 s2 n A B t u hA hB ht hu)
+
+| nice_eq_conv Γ s T1 T2 t1 t2 ht hT :
+    nice_eq_term _ _ _ _ _ ht ->
+    nice_eq_term _ _ _ _ _ hT ->
+    nice_eq_term _ _ _ _ _ (eq_conv Σ Γ s T1 T2 t1 t2 ht hT)
+
+| nice_cong_Prod Γ n1 n2 A1 A2 B1 B2 s1 s2 hA hB hB1 hB2 :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_eq_term _ _ _ _ _ (cong_Prod Σ Γ n1 n2 A1 A2 B1 B2 s1 s2 hA hB hB1 hB2)
+
+| nice_cong_Lambda Γ n1 n2 n' A1 A2 B1 B2 t1 t2 s1 s2 hA hB ht hB1 hB2 ht1 ht2 :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_eq_term _ _ _ _ _ ht ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_typing _ _ _ _ ht1 ->
+    nice_typing _ _ _ _ ht2 ->
+    nice_eq_term _ _ _ _ _ (cong_Lambda Σ Γ n1 n2 n' A1 A2 B1 B2 t1 t2 s1 s2 hA hB ht hB1 hB2 ht1 ht2)
+
+| nice_cong_App Γ n1 n2 s1 s2 t1 t2 A1 A2 B1 B2 u1 u2 hA hB ht hu hB1 hB2 ht1 ht2 hu1 hu2 :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_eq_term _ _ _ _ _ ht ->
+    nice_eq_term _ _ _ _ _ hu ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_typing _ _ _ _ ht1 ->
+    nice_typing _ _ _ _ ht2 ->
+    nice_typing _ _ _ _ hu1 ->
+    nice_typing _ _ _ _ hu2 ->
+    nice_eq_term _ _ _ _ _ (cong_App Σ Γ n1 n2 s1 s2 t1 t2 A1 A2 B1 B2 u1 u2 hA hB ht hu hB1 hB2 ht1 ht2 hu1 hu2)
+
+| nice_cong_Sum Γ n1 n2 A1 A2 B1 B2 s1 s2 hA hB hB1 hB2 :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_eq_term _ _ _ _ _ (cong_Sum Σ Γ n1 n2 A1 A2 B1 B2 s1 s2 hA hB hB1 hB2)
+
+| nice_cong_Pair Γ n A1 A2 B1 B2 u1 u2 v1 v2 s1 s2 hA hB hu hv hB1 hB2 hu1 hu2 hv1 hv2 :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_eq_term _ _ _ _ _ hu ->
+    nice_eq_term _ _ _ _ _ hv ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_typing _ _ _ _ hu1 ->
+    nice_typing _ _ _ _ hu2 ->
+    nice_typing _ _ _ _ hv1 ->
+    nice_typing _ _ _ _ hv2 ->
+    nice_eq_term _ _ _ _ _ (cong_Pair Σ Γ n A1 A2 B1 B2 u1 u2 v1 v2 s1 s2 hA hB hu hv hB1 hB2 hu1 hu2 hv1 hv2)
+
+| nice_cong_Pi1 Γ nx ny A1 A2 B1 B2 s1 s2 p1 p2 hp hA hB hB1 hB2 hp1 hp2 :
+    nice_eq_term _ _ _ _ _ hp ->
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_eq_term _ _ _ _ _ (cong_Pi1 Σ Γ nx ny A1 A2 B1 B2 s1 s2 p1 p2 hp hA hB hB1 hB2 hp1 hp2)
+
+| nice_cong_Pi2 Γ nx ny A1 A2 B1 B2 s1 s2 p1 p2 hp hA hB hB1 hB2 hp1 hp2 :
+    nice_eq_term _ _ _ _ _ hp ->
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hB ->
+    nice_typing _ _ _ _ hB1 ->
+    nice_typing _ _ _ _ hB2 ->
+    nice_eq_term _ _ _ _ _ (cong_Pi2 Σ Γ nx ny A1 A2 B1 B2 s1 s2 p1 p2 hp hA hB hB1 hB2 hp1 hp2)
+
+| nice_cong_Eq Γ s A1 A2 u1 u2 v1 v2 hA hu hv :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hu ->
+    nice_eq_term _ _ _ _ _ hv ->
+    nice_eq_term _ _ _ _ _ (cong_Eq Σ Γ s A1 A2 u1 u2 v1 v2 hA hu hv)
+
+| nice_cong_Refl Γ s A1 A2 u1 u2 hA hu :
+    nice_eq_term _ _ _ _ _ hA ->
+    nice_eq_term _ _ _ _ _ hu ->
+    nice_eq_term _ _ _ _ _ (cong_Refl Σ Γ s A1 A2 u1 u2 hA hu)
+
+| nice_reflection Γ A u v e he :
+    nice_typing _ _ _ _ he ->
+    nice_eq_term _ _ _ _ _ (reflection Σ Γ A u v e he)
 
 with nice_wf Σ : forall Γ, wf Σ Γ -> Type :=
 | nice_nil : nice_wf _ _ (wf_nil Σ)
